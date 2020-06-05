5b689f2793d518ebf67c1e7794986e95af7c83aa
uli <uli@uli.uli>
2020-06-05 15:44:26 +0200

    Improve ntpq parsing
    A more elegant way to parse the output of the "ntpq -rv" command which makes preselecting the correct version unnecessary and should be more robust to any format changes.

--- snmp/ntp-server.sh
+++ snmp/ntp-server.sh
@@ -2,14 +2,6 @@
 # Please make sure the paths below are correct.
 # Alternatively you can put them in $0.conf, meaning if you've named
 # this script ntp-client.sh then it must go in ntp-client.sh.conf .
-#
-# NTPQV output version of "ntpq -c rv" 
-# p1 DD-WRT and some other outdated linux distros
-# p11 FreeBSD 11 and any linux distro that is up to date
-#
-# If you are unsure, which to set, run this script and make sure that
-# the JSON output variables match that in "ntpq -c rv".
-#
 BIN_NTPD='/usr/bin/env ntpd'
 BIN_NTPQ='/usr/bin/env ntpq'
 BIN_NTPDC='/usr/bin/env ntpdc'
@@ -18,7 +10,6 @@ BIN_TR='/usr/bin/env tr'
 BIN_CUT='/usr/bin/env cut'
 BIN_SED="/usr/bin/env sed"
 BIN_AWK='/usr/bin/env awk'
-NTPQV="p11"
 ################################################################
 # Don't change anything unless you know what are you doing     #
 ################################################################
@@ -26,26 +17,15 @@ CONFIG=$0".conf"
 if [ -f $CONFIG ]; then
     . $CONFIG
 fi
-VERSION=1
+VERSION=2
 
-STRATUM=`$BIN_NTPQ -c rv | $BIN_GREP -Eow "stratum=[0-9]+" | $BIN_CUT -d "=" -f 2`
-
-# parse the ntpq info that requires version specific info
-NTPQ_RAW=`$BIN_NTPQ -c rv | $BIN_GREP jitter | $BIN_SED 's/[[:alpha:]=,_]/ /g'`
-if [ $NTPQV = "p11" ]; then
-	OFFSET=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $3}'`
-	FREQUENCY=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $4}'`
-	SYS_JITTER=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $5}'`
-	CLK_JITTER=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $6}'`
-	CLK_WANDER=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $7}'`
-fi
-if [ $NTPQV = "p1" ]; then
-	OFFSET=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $2}'`
-	FREQUENCY=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $3}'`
-	SYS_JITTER=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $4}'`
-	CLK_JITTER=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $5}'`
-	CLK_WANDER=`echo $NTPQ_RAW | $BIN_AWK -F ' ' '{print $6}'`
-fi
+NTPQ_RAW=`$BIN_NTPQ -c rv | $BIN_TR ',' '\n' | $BIN_AWK '{$1=$1};1'`
+OFFSET=`echo $NTPQ_RAW | $BIN_GREP -oP '(?<=offset=).\S*'`
+STRATUM=`echo $NTPQ_RAW | $BIN_GREP -oP '(?<=stratum=).\S*'`
+FREQUENCY=`echo$NTPQ_RAW | $BIN_GREP -oP '(?<=frequency=).\S*'`
+SYS_JITTER=`echo $NTPQ_RAW | $BIN_GREP -oP '(?<=sys_jitter=).\S*'`
+CLK_JITTER=`echo $NTPQ_RAW | $BIN_GREP -oP '(?<=clk_jitter=).\S*'`
+CLK_WANDER=`echo $NTPQ_RAW | $BIN_GREP -oP '(?<=clk_wander=).\S*'`
 
 VER=`$BIN_NTPD --version`
 if [ "$VER" = '4.2.6p5' ]; then
